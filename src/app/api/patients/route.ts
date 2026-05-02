4ec197290639d77c51c1f4525708789eba0b9099
Ndèye Félicité Diop <diopfelicite11@gmail.com>
2026-05-02 00:16:39 +0000

    fusion sur le contenus de route.ts

--- src/app/api/patients/route.ts
+++ src/app/api/patients/route.ts
@@ -1,7 +1,18 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@lib/auth";
+
 export async function GET() {
 try {
+const session = await getServerSession(authOptions);
+if (!session) {
+return NextResponse.json({
+error: "Non autorisé",
+status: 401
+});
+}
+
 const patients = await prisma.patient.findMany({
 orderBy: { createdAt: "desc" },
 });
@@ -13,8 +24,17 @@ return NextResponse.json(
 );
 }
 }
+
 export async function POST(request: Request) {
 try {
+const session = await getServerSession(authOptions);
+if (!session) {
+return NextResponse.json({
+error: "Non autorisé",
+status: 401
+});
+}
+
 const body = await request.json();
 const patient = await prisma.patient.create({
 data: {
